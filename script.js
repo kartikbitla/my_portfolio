62d5c827b955b009693649470b0acb9146282335
Kartik Bitla <bitlakartik@gmail.com>
2023-10-25 08:07:25 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,103 +1,55 @@
-let fnameEl = document.getElementById("fname");
-let lnameEl = document.getElementById("lname");
-let emailEl = document.getElementById("email");
+let homePageEl = document.getElementById("homepage-content");
+let homePageImg = document.getElementById("hompage-img");
+
+let aboutEle = document.getElementById("about");
+let aboutContent = document.getElementById("about-content");
+
+
+let workEle = document.getElementById("work");
+let workContent = document.getElementById("work-content");
+
+
 let contactEl = document.getElementById("contact");
-let studentIdEl = document.getElementById("student_id");
-let feedbackEl = document.getElementById("feedback");
-let submitEl = document.getElementById("submit");
-let formsEl = document.getElementById("forms");
-let headEl = document.getElementById("head");
-let another_feedbackEl = document.getElementById("another_feedback");
-let errorMsgEl = document.getElementById("errorMsg");
+let contactContent = document.getElementById("contact-content");
 
 
-let eFname = document.getElementById("e-fname");
-let eLname = document.getElementById("e-lname");
-let eEmail = document.getElementById("e-email");
-let eContact = document.getElementById("e-contact");
-let eStudentId = document.getElementById("e-studentId");
+aboutEle.addEventListener("click", function() {
+    let backAbout = document.getElementById("about-back");
+    aboutContent.style.display = "block";
+    homePageEl.style.display = "none";
+    homePageImg.style.display = "none";
 
-another_feedbackEl.style.display = "none";
-
-
-
-function isEmpty(){
-    if(fnameEl.value==="" || lnameEl.value==="" || emailEl.value==="" || contactEl.value==="" || studentIdEl.value===""){
-        whichFieldIsEmpty();
-        return true;
-    }else{
-        return false;
-    }
-}
-
-function whichFieldIsEmpty(){
-    if(fnameEl.value===""){
-        eFname.style.display = "block"; 
-    }else{
-        eFname.style.display = "none"
-    }
-
-    if(lnameEl.value===""){
-        eLname.style.display = "block";
-    }else{
-        eLname.style.display = "none";
-    }
-
-    if(emailEl.value===""){
-        eEmail.style.display = "block";
-    }else{
-        eEmail.style.display = "none";
-    }
-    
-    if(contactEl.value===""){
-        eContact.style.display = "block";
-    }else{
-        eContact.style.display = "none";
-    }
-    
-    if(studentIdEl.value===""){
-        eStudentId.style.display = "block";
-    }else{
-        eStudentId.style.display = "none";
-    }
-}
-
-function emptyEveryInput(){
-    fnameEl.value = "";
-    lnameEl.value = "";
-    emailEl.value = "";
-    contactEl.value = ""; 
-    studentIdEl.value = ""; 
-    feedbackEl.value = ""; 
-
-    eFname.style.display = "none";
-    eLname.style.display = "none";
-    eEmail.style.display = "none";
-    eContact.style.display = "none";
-    eStudentId.style.display = "none";
-    errorMsgEl.style.display = "none";
-    
-}
+    backAbout.addEventListener("click", function() {
+        aboutContent.style.display = "none";
+        homePageEl.style.display = "block";
+        homePageImg.style.display = "block";
+    });
+});
 
 
-submitEl.addEventListener("click", function(){   
-    if(isEmpty()){
-        errorMsgEl.style.display = "block";
-        formsEl.style.display = "block";
-    }else{
-        formsEl.style.display = "none";
-        another_feedbackEl.style.display = "block";
-        headEl.textContent = "Thank you for your Valuable feedback!";
+workEle.addEventListener("click", function() {
+    let backWork = document.getElementById("work-back");
+    workContent.style.display = "block";
+    homePageEl.style.display = "none";
+    homePageImg.style.display = "none";
 
-        another_feedbackEl.addEventListener("click", function(){
-            formsEl.style.display = "block";
-            another_feedbackEl.style.display = "none";
-            headEl.textContent = "Help us Improve";
-            window.history.go(-1);
+    backWork.addEventListener("click", function() {
+        workContent.style.display = "none";
+        homePageEl.style.display = "block";
+        homePageImg.style.display = "block";
+    });
+});
 
-            emptyEveryInput();
 
-        });
-    }   
-    
+contactEl.addEventListener("click", function() {
+    let backContact = document.getElementById("contact-back");
+    contactContent.style.display = "block";
+    homePageEl.style.display = "none";
+    homePageImg.style.display = "none";
+
+    backContact.addEventListener("click", function() {
+        contactContent.style.display = "none";
+        homePageEl.style.display = "block";
+        homePageImg.style.display = "block";
+    });
 });
